68dca96b5c81888c89f84fefc4420eff77fcce9a
Kim BeomJung <kbjoong@daum.net>
2022-02-11 16:35:19 +0900

    functions

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,30 @@
-// object
+// arguments
+    // 함수에게 데이터를 전달하는 변수
+function sayHello(nameOfPerson, age) {
+    console.log("Hello! my name is " + nameOfPerson + " and I'm " + age);
+}
 
-const player = {name:"nico", points:10, fat:true,};
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
 
-console.log(player);
-player.lastname = "potato";
-player.points = player.points + 15;
-console.log(player);
+function plus(a, b) {
+    console.log(a + b);
+}
+
+plus(8, 60);
+
+function divide(a, b) {
+    console.log(a / b);
+}
+divide(98, 20);
+
+const player = {
+    name:"nico", 
+    sayHello:function(otherPersonName) {
+        console.log("Hello! " + otherPersonName + " nice to meet you!");
+    },
+};
+
+console.log(player.name);
+player.sayHello("lynn");
