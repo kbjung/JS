215072545107fddf7caa4de9c1589670079e5710
Kim BeomJung <kbjoong@daum.net>
2022-02-12 15:16:42 +0900

    총 정리
    - data type
    - const, let, var
    - variable
    - object / array
    - function

--- momentum/app.js
+++ momentum/app.js
@@ -1,30 +1,37 @@
-// arguments
-    // 함수에게 데이터를 전달하는 변수
-function sayHello(nameOfPerson, age) {
-    console.log("Hello! my name is " + nameOfPerson + " and I'm " + age);
-}
+const toBuy = ['tomato', 'apple', 'potato'];
 
-sayHello("nico", 10);
-sayHello("dal", 23);
-sayHello("lynn", 21);
+console.log(toBuy);
+toBuy[2] = 'milk';
+console.log(toBuy);
+toBuy.push('pork');
+console.log(toBuy);
+
+const player ={
+    name:"nico",
+    age:10,
+};
+console.log(player, console);
+player.weight = 'increase';
+console.log(player);
 
 function plus(a, b) {
     console.log(a + b);
-}
-
-plus(8, 60);
-
-function divide(a, b) {
-    console.log(a / b);
-}
-divide(98, 20);
-
-const player = {
-    name:"nico", 
-    sayHello:function(otherPersonName) {
-        console.log("Hello! " + otherPersonName + " nice to meet you!");
-    },
 };
 
-console.log(player.name);
-player.sayHello("lynn");
+plus(1,2);
+plus(2,3);
+
+const calcurator = {
+    add: function(a, b){ 
+        console.log(a + b);
+    },
+    min: function(a, b){
+        console.log(a - b);
+    },
+    power: function(a, b){
+        console.log(a ** b);
+    },
+}
+calcurator.add(2,3);
+calcurator.min(2,3);
+calcurator.power(2,3);
