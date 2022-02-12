034a80801d8235a9365d78a0a99c1943d91aff4e
Kim BeomJung <kbjoong@daum.net>
2022-02-12 20:46:14 +0900

    returns

--- momentum/app.js
+++ momentum/app.js
@@ -1,37 +1,33 @@
-const toBuy = ['tomato', 'apple', 'potato'];
+const age = 80;
 
-console.log(toBuy);
-toBuy[2] = 'milk';
-console.log(toBuy);
-toBuy.push('pork');
-console.log(toBuy);
-
-const player ={
-    name:"nico",
-    age:10,
-};
-console.log(player, console);
-player.weight = 'increase';
-console.log(player);
-
-function plus(a, b) {
-    console.log(a + b);
+function calculatorAgeOfKr(age) {
+    return age + 2;
 };
 
-plus(1,2);
-plus(2,3);
+const krAge = calculatorAgeOfKr(age);
+console.log(krAge);
 
-const calcurator = {
-    add: function(a, b){ 
-        console.log(a + b);
+const calculator = {
+    plus:function(a, b) {
+        return a + b;
     },
-    min: function(a, b){
-        console.log(a - b);
+    minus:function(a, b) {
+        return a - b;
     },
-    power: function(a, b){
-        console.log(a ** b);
+    times:function(a, b) {
+        return a * b;
+    },
+    divide:function(a, b) {
+        return a / b;
+    },
+    power:function(a, b) {
+        return a ** b;
     },
 }
-calcurator.add(2,3);
-calcurator.min(2,3);
-calcurator.power(2,3);
+
+const plusResult = calculator.plus(3, 5);
+const minusResult = calculator.minus(plusResult, 6);
+const timesResult = calculator.times(minusResult, 5);
+const divideResult = calculator.divide(timesResult, 2);
+const powerResult = calculator.power(divideResult, 3);
+console.log(powerResult);
