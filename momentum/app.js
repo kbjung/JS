0282d63f14ad51d1299866ba620127f237e1a617
Kim BeomJung <kbjoong@daum.net>
2022-02-16 15:38:56 +0900

    Events

--- momentum/app.js
+++ momentum/app.js
@@ -1,3 +1,7 @@
-const title = document.querySelectorAll('.hello h1');
+const title = document.querySelector('div.hello:first-child h1');
 
-console.log(title);
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
