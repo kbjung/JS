90d479b54a0940801fef2a2bf9f06628b4836c09
Kim BeomJung <kbjoong@daum.net>
2022-02-18 11:51:04 +0900

    Input Values

--- momentum/app.js
+++ momentum/app.js
@@ -1,16 +1,8 @@
-const h1 = document.querySelector('div.hello:first-child h1');
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-    // const clickedClass = "clicked";
-    // if (h1.classList.contains(clickedClass)) {
-    // h1.classList.remove(clickedClass);
-    // }
-    // else {
-    //     h1.classList.add(clickedClass);
-    // }
-    
-    h1.classList.toggle("clicked");
+function loginButtonClick() {
+    console.log("hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
-// 동일한 기능 h1.onclick = handleTitleClick;
+loginButton.addEventListener("click", loginButtonClick);
