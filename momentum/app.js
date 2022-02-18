634507e0be67cc996eb1c049db730bac13b90d6b
Kim BeomJung <kbjoong@daum.net>
2022-02-18 16:21:41 +0900

    Events 2

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 브라우저의 기본 동작을 막는 코드
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
