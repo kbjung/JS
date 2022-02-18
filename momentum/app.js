404e944bf3a4708d67f47fb94a58170fd33ea448
Kim BeomJung <kbjoong@daum.net>
2022-02-18 16:56:45 +0900

    Saving Username

--- momentum/app.js
+++ momentum/app.js
@@ -1,16 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 브라우저의 기본 동작을 막는 코드
-    console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`; //동일한 결과 string안에 변수 값을 표현하는 방법
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
