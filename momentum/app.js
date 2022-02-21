3c1180f9161dc64acc83660403291ef3f2f9f8b2
Kim BeomJung <kbjoong@daum.net>
2022-02-21 12:08:27 +0900

    Loading Username

--- momentum/app.js
+++ momentum/app.js
@@ -3,15 +3,29 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 브라우저의 기본 동작을 막는 코드
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
-    greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`; //동일한 결과 string안에 변수 값을 표현하는 방법
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    // show the greetings
+    paintGreetings(savedUsername);
+}
