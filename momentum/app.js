affdc0076f787885cfacc0377786adb8c55aa9be
Kim BeomJung <kbjoong@daum.net>
2022-02-21 11:35:35 +0900

    Saving Username

--- momentum/app.js
+++ momentum/app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
     event.preventDefault(); // 브라우저의 기본 동작을 막는 코드
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.innerText = "Hello " + username;
     greeting.innerText = `Hello ${username}`; //동일한 결과 string안에 변수 값을 표현하는 방법
     greeting.classList.remove(HIDDEN_CLASSNAME);
